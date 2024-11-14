ed627e6f22d3a7ecfa8fb8eb750fc86ae4e9507f
Lucàs <86352901+LucasVbr@users.noreply.github.com>
2024-11-14 17:32:15 +0100

    feat: Add .gitpod.yml

--- index.ts
+++ index.ts
@@ -39,6 +39,6 @@ function isValidDateParam(date: String): boolean {
       || date.match(/\d+/) !== null;
 }
 
-app.listen(80, () => {
+app.listen(3000, () => {
   console.log('Server is running on port 3000');
 });
